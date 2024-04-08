07a6fc1b75e708aa26ba152f95b4e19455bdc5e9
Some_Shit_Creator <campanula_barbata@riseup.net>
2024-04-08 20:43:09 +0200

    adding libraries

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 <html>
     <head>
         <title>Списк матчей</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        
+        <script src="/static/js/jquery-3.7.1.js"></script>
         
     </head>
 
