7737c5c86e2eebac09c7cf351eb87298a8815f8a
Some_Shit_Creator <campanula_barbata@riseup.net>
2024-04-08 20:39:32 +0200

    add html to index

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <?php
-require("commands.php");
+    require("commands.php");
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Списк матчей</title>
+        
+    </head>
 
-foreach($rounds as $round_count => $round) { 
-    echo "Круг ".$round_count."\n\n";
-    foreach(array_chunk($round, 10) as $tour_count => $tour) {
-        echo "Тур ".$tour_count."\n\n";
-        foreach($tour as $round) {
-            echo $round[0]."-".$round[1]."\n\n";
-        }
-    }
-}
+    <body>
+    
+        <div>
+            <?php foreach($rounds as $round_count => $round) { ?>    
+            <div>    
+                <h1>Круг <?= $round_count ?></h1>
+                
+                <?php foreach(array_chunk($round, 10) as $tour_count => $tour) { ?>
+                <div>
+                        <h2>Тур <?= $tour_count ?></h2  >
+                    <table>
+                        <tr>
+                            <th>Хозяева</th>
+                            <th></th>
+                            <th>Гости</th>
+                        </tr>
+                        <?php foreach($tour as $round) { ?>
+                            <tr>
+                                <td> <?= $round[0] ?></td>
+                                <td>-</td>
+                                <td><?= $round[1] ?></td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                </div>
+                <?php } ?>
+            </div>
+            <?php } ?>
+        </div>
+    </body>
+</html>
